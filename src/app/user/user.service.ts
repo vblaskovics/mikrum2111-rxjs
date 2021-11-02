1011453b49e0ffd48ec43e222cde26c69fdc3178
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-11-02 15:48:41 +0100

    Add currentUser stream to user service

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  /** 
+   * Usage: 
+   * 
+   * let user1:User = new User({name:'Peter', email:'peter@gmail.com'});
+   * this.userService.currentUser.next(user1);
+   * 
+   * Elsewhere:
+   * 
+   * this.userService.currentUser.subscribe((user) => {
+   *    console.log('Current user', user.name);
+   * })
+  */
+  currentUser: Subject<User> = new BehaviorSubject<User>(new User());
+
+  public setCurrentUser(newUser: User){
+    this.currentUser.next(newUser);
+  }
+}
