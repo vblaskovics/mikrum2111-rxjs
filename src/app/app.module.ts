aacb75a2ec1063ecfb2f5025f5c65d6660d04260
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-11-02 15:52:14 +0100

    Add navbar and update stlye

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatNavBarComponent
   ],
   imports: [
     BrowserModule
