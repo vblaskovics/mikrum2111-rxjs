3959f0a98120b5c12c40e9e85b90f9263fc8bc86
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-11-03 11:49:15 +0100

    Add message creation workflow

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, map, scan, shareReplay } from 'rxjs/operators';
 import { Thread } from '../thread/thread.model';
 import { User } from '../user/user.model';
 import { Message } from './message.model';
+
+const initialMessages: Message[] = [];
+
+interface IMessagesOperation extends Function {
+  (messages: Message[]): Message[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +18,39 @@ export class MessagesService {
 
   newMessages: Subject<Message> = new Subject<Message>();
 
-  constructor() { }
+  messages: Observable<Message[]>;
+
+  updates: Subject<any> = new Subject<any>();
+
+  create: Subject<Message> = new Subject<Message>();
+
+  constructor() {
+    this.messages = this.updates.pipe(
+      scan((messages: Message[], operation: IMessagesOperation) => {
+        return operation(messages);
+      }, initialMessages),
+      shareReplay(1)
+    );
+
+    this.create.pipe(
+      map((message: Message): IMessagesOperation => {
+        return (messages: Message[]) => {
+          return messages.concat(message);
+        };
+      })
+    ).subscribe(this.updates);
+
+    this.newMessages.subscribe(this.create);
+
+    // PÉLDA HASZNÁLAT:
+    // let myMessage = new Message({text:"hello"});
+
+    // this.updates.next( (messages:Message[]): Message[] => {
+    //   return messages.concat(myMessage);
+    // });
+
+
+  }
 
   addMessage(message: Message) {
     this.newMessages.next(message);
